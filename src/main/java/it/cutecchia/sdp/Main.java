77f0c723399e5b2f936a54726cefbef8fb2d688f
Marco Cutecchia <marco.cutecchia@outlook.it>
2021-05-28 18:13:57 +0200

    Update gradle syntax, delete useless stuff

---
+++ src/main/java/it/cutecchia/sdp/Main.java
@@ -0,0 +1,7 @@
+package it.cutecchia.sdp;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/it/cutecchia/sdp/SimpleTest.java
@@ -0,0 +1,12 @@
+package it.cutecchia.sdp;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+  @Test
+  public void simpleTest() {
+    assertThat(1 + 1).isEqualTo(2);
+  }
+}
